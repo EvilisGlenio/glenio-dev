506818d4dea4b8f3686c518df5604180cc730465
Evilis Glenio <glenio.developer@gmail.com>
2023-11-28 13:29:32 -0300

    Update of my Header. Dinamic Content.

---
+++ app/components/Header/content.tsx
@@ -0,0 +1,17 @@
+import Link from "next/link";
+
+interface contentProps {
+  index: number;
+  href: string;
+  menu: string;
+}
+
+export const Content = ({ href, index, menu }: contentProps) => {
+  return (
+    <li key={index}>
+      <Link className="opacity-50 " href={href}>
+        {menu}
+      </Link>
+    </li>
+  );
+};
--- app/components/Header/page.tsx
+++ app/components/Header/page.tsx
@@ -1,3 +1,35 @@
+import { Content } from "./content";
+
 export const Header = () => {
-  return <div>HEADER</div>;
+  const ContentA = [
+    {
+      index: 1,
+      href: "/",
+      menu: "ABOUT",
+    },
+    {
+      index: 2,
+      href: "/",
+      menu: "PROJECTS",
+    },
+    {
+      index: 3,
+      href: "/",
+      menu: "CONTACT",
+    },
+    {
+      index: 4,
+      href: "/",
+      menu: "RESUME",
+    },
+  ];
+  return (
+    <section className="flex w-screen items-center p-6">
+      <ul className="flex w-1/3 items-center justify-between ">
+        {ContentA.map(({ href, index, menu }) => {
+          return <Content index={index} href={href} key={index} menu={menu} />;
+        })}
+      </ul>
+    </section>
+  );
 };
