b9154fa792b6bf5ef5ecd464794be846219f242d
Evilis Glenio <glenio.developer@gmail.com>
2023-11-29 12:14:43 -0300

    Hover incremented in the menus

--- app/components/Header/content.tsx
+++ app/components/Header/content.tsx
@@ -9,7 +9,7 @@ interface contentProps {
 export const Content = ({ href, index, menu }: contentProps) => {
   return (
     <li key={index}>
-      <Link className="opacity-50 " href={href}>
+      <Link className="opacity-50 hover:opacity-100 " href={href}>
         {menu}
       </Link>
     </li>
