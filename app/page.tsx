42df14748bd17716543488bc460499cb194d4532
Evilis Glenio <glenio.developer@gmail.com>
2023-11-29 12:13:49 -0300

    Page updated and Header removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import { Header } from "./components/Header/page";
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
-      <Header />
-    </main>
+    <main className="flex min-h-screen w-screen flex-col items-center justify-between"></main>
   );
 }
