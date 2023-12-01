fb678f02af15a71affee4d6ba947e332fd5f508b
Evilis Glenio <glenio.developer@gmail.com>
2023-12-01 12:26:56 -0300

    tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,15 +7,13 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      // backgroundImage: {
-      //   "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-      //   "gradient-conic":
-      //     "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      // },
-    },
+    extend: {},
   },
   darkMode: "class",
+  fontFamily: {
+    sans: ["Inter", "sans-serif"],
+    serif: ["Merriweather", "serif"],
+  },
   plugins: [],
 };
 export default config;
