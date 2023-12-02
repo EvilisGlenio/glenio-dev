0f87438d1271c7877c3c747ee27fce5ead0e2e28
Evilis Glenio <glenio.developer@gmail.com>
2023-12-02 18:47:56 -0300

    Footer component created

---
+++ app/components/Footer/page.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <section className="flex w-full items-center justify-center dark:bg-zinc-950 p-9">
+      <p className="opacity-50 dark:text-gray-50">©2023 Evilis Gomes</p>
+    </section>
+  );
+};
+
+export default Footer;
