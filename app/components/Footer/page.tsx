9cd59d100708a559e8ebccc9c226e3f8a4398879
Evilis Glenio <glenio.developer@gmail.com>
2023-12-12 10:59:19 -0300

    Adjustment of the font-size responsive

--- app/components/Footer/page.tsx
+++ app/components/Footer/page.tsx
@@ -1,7 +1,9 @@
 const Footer = () => {
   return (
     <section className="flex w-full items-center justify-center dark:bg-zinc-950 p-9">
-      <p className="text-gray-700 dark:text-gray-400">©2023 Evilis Gomes</p>
+      <p className="text-gray-700 dark:text-gray-400 text-md sm:text-sm max-sm:text-xs">
+        ©2023 Evilis Gomes
+      </p>
     </section>
   );
 };
