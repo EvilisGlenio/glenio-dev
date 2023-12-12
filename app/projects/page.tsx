ddb6e1a60f4f658b37b3dc7fce741095e0c44c09
Evilis Glenio <glenio.developer@gmail.com>
2023-12-12 12:14:51 -0300

    Initialization of the Projects page

---
+++ app/projects/page.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <main className="flex w-full h-full flex-col items-center dark:bg-zinc-950 pt-32">
+      <section className="flex flex-col w-2/3 max-w-7xl items-center justify-start gap-9 md:gap-7 max-sm:gap-5">
+        <h1 className="text-5xl max-lg:text-4xl max-md:text-3xl max-sm:text-xl dark:text-gray-50">
+          Conectando sonhos por meio da tecnologia
+        </h1>
+
+        <div className="flex flex-col gap-6 max-md:gap-4 max-sm:gap-3 max-md:text-xs text-gray-700 dark:text-gray-400 "></div>
+      </section>
+    </main>
+  );
+};
+
+export default Projects;
