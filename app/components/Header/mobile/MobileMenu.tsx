e0aa93889b15c3b1de08ddc9b7fa984bdf7f70fe
Evilis Glenio <glenio.developer@gmail.com>
2023-12-12 12:14:10 -0300

    Correction of text menu color

--- app/components/Header/mobile/MobileMenu.tsx
+++ app/components/Header/mobile/MobileMenu.tsx
@@ -22,7 +22,7 @@ const MobileMenu = () => {
         type="button"
         data-drawer-hide="drawer-navigation"
         aria-controls="drawer-navigation"
-        className="bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-zinc-800 dark:hover:text-white"
+        className="bg-transparent hover:bg-gray-200 dark:text-gray-100 rounded-lg text-sm p-1.5 absolute top-2.5 end-2.5 inline-flex items-center dark:hover:bg-zinc-800 dark:hover:text-white"
         onClick={() => changeCurrentMode(!currentMode)}
       >
         <svg
