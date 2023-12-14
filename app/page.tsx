d5b88a46f2d84db4966be827ba43e4a4af3a1e04
Evilis Glenio <glenio.developer@gmail.com>
2023-12-14 10:46:27 -0300

    resuming my description

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex w-full h-full flex-col items-center dark:bg-zinc-950 pt-32">
       <section className="flex flex-col w-2/3 max-w-7xl items-center max-sm:justify-center sm:justify-start gap-9 md:gap-7 max-sm:gap-5">
-        <div className="flex w-full max-sm:flex-col max-sm:items-center max-sm:gap-3 gap-1 sm:flex-row-reverse sm:items-center sm:justify-between  ">
+        <div className="flex w-full max-sm:flex-col max-sm:items-center max-sm:gap-3 gap-2 sm:flex-row-reverse sm:items-center sm:justify-between  ">
           <div className="relative -z-0 flex-none max-sm:w-20 sm:w-24 md:w-28 max-sm:h-20 sm:h-24 md:h-28">
             <Image
               src="https://avatars.githubusercontent.com/u/69771563?v=4"
@@ -31,19 +31,16 @@ export default function Home() {
         </ul>
         <div className="flex flex-col max-sm:text-center gap-6 max-md:gap-4 max-sm:gap-3 max-md:text-xs text-gray-700 dark:text-gray-400 ">
           <p>
-            Desenvolvedor Frontend dedicado com experiência sólida em projetos
-            web inovadores. Minhas habilidades incluem o desenvolvimento
-            eficiente de aplicações com foco em React e Node, bem como a
-            implementação de soluções avançadas com Next.js. Ao longo da minha
-            jornada, foquei em aprimorar minha lógica de programação, buscando
-            constantemente elevados padrões de qualidade.
+            Desenvolvedor dedicado com experiência sólida em projetos
+            inovadores, especializado em React, Node e Next.js. Busco
+            constantemente aprimorar minha lógica de programação, mantendo
+            elevados padrões de qualidade em cada desenvolvimento.
           </p>
           <p>
-            Minha paixão por aprendizado contínuo e crescimento me impulsiona a
-            enfrentar desafios que ampliem minha capacidade de resolver
-            problemas e aprimorar minhas habilidades técnicas. Estou confiante
-            de que minha experiência e comprometimento com a excelência me
-            tornam um ativo valioso para equipes de desenvolvimento.
+            Minha paixão por aprendizado constante me impulsiona a enfrentar
+            desafios, aprimorando minhas habilidades técnicas. Confio que minha
+            experiência e busca pela excelência são ativos valiosos para equipes
+            de desenvolvimento.
           </p>
           <p>
             Entusiasmado com oportunidades de contribuir para projetos
