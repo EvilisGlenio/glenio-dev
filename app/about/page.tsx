e8df8128e6b1643c0e721226962bb98f0861f384
Evilis Glenio <glenio.developer@gmail.com>
2023-12-14 11:06:23 -0300

    resuming my about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -11,28 +11,23 @@ const About = () => {
 
         <div className="flex flex-col gap-6 max-md:gap-4 max-sm:gap-3 max-md:text-xs text-gray-700 dark:text-gray-400 ">
           <p>
-            Sou apaixonado por programação e pelo impacto que a tecnologia pode
-            ter na vida das pessoas. Minha missão é utilizar minhas habilidades
-            e conhecimentos para criar experiências inovadoras e envolventes que
-            conectem as pessoas aos seus objetivos. Como desenvolvedor, estou
-            sempre aprendendo e crescendo, buscando sempre melhorar minha
-            técnica e me manter atualizado com as últimas tendências e
-            tecnologias.
+            Apaixonado por programação e pelo impacto da tecnologia na vida das
+            pessoas, minha missão é criar experiências inovadoras que conectem
+            as pessoas aos seus objetivos. Como desenvolvedor, estou sempre
+            aprendendo e crescendo, buscando melhorar minha técnica e me manter
+            atualizado com as últimas tendências e tecnologias.
           </p>
           <p>
-            Atualmente, colocando minhas habilidades em prática criando
-            aplicações do mundo real que gere resultados. Como desenvolvedor
-            junior, estou ansioso para adquirir experiência em um ambiente
-            profissional e trabalhar com outros desenvolvedores para criar
-            projetos incríveis.
+            Aplicando minhas habilidades em projetos do mundo real, busco
+            experiência profissional como desenvolvedor junior, ansioso para
+            colaborar com outros profissionais na criação de projetos
+            impactantes.
           </p>
           <p>
-            Fora do trabalho, adoro viajar de moto e curtir animes na companhia
-            de minha esposa. Acredito que ter uma vida equilibrada é importante,
-            e tento cuidar da minha saúde física e mental, mantendo-me ativo e
-            passando tempo com as pessoas que amo. Estou animado para ver para
-            onde minha carreira me levará e continuar aprendendo e crescendo
-            como desenvolvedor.
+            Fora do trabalho, sou apaixonado por viajar de moto, assistir animes
+            com minha esposa e manter um equilíbrio entre minha saúde física e
+            mental. Ansioso para ver o rumo da minha carreira, continuando a
+            aprender e crescer como desenvolvedor.
           </p>
         </div>
       </section>
